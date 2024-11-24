2f566cba7064332b24ed82dc7dbcaa5bb2612961
KauaiMoho <113269300+KauaiMoho@users.noreply.github.com>
2024-11-24 11:57:56 -0800

    vader

--- process_semantics.py
+++ process_semantics.py
@@ -1,12 +1,19 @@
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-import pandas as pd
-import string
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
 
-lemmatizer = WordNetLemmatizer()
-analyzer = SentimentIntensityAnalyzer()
+import pandas as pd
+from finvader import finvader                         
+
+# from transformers import BertTokenizer, BertForSequenceClassification
+# from transformers import pipeline
+
+# model_name = 'ProsusAI/finbert'
+# tokenizer = BertTokenizer.from_pretrained(model_name)
+# model = BertForSequenceClassification.from_pretrained(model_name)
+# finbert_pipeline = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+
+#VADER Scores
+# positive sentiment: compound score >= 0.05
+# neutral sentiment: (compound score > -0.05) and (compound score < 0.05)
+# negative sentiment: compound score <= -0.05
 
 dax_ft = pd.read_csv('dax_ft.csv')
 dax_investing = pd.read_csv('dax_investing.csv', parse_dates=["date"])
@@ -27,18 +34,6 @@ dax_yahoo.set_index('date', inplace=True)
 dax_merged = pd.concat([dax_ft, dax_investing, dax_tradingview, dax_yahoo])
 dax_merged.sort_index(inplace=True, ascending=False)
 
-def preprocess_text(text):
-    text = text.lower()
-    text = text.translate(str.maketrans('', '', string.punctuation))
-    tokens = word_tokenize(text)
-    stop_words = set(stopwords.words('english'))
-    tokens = [word for word in tokens if word not in stop_words]
-    tokens = [lemmatizer.lemmatize(word) for word in tokens]
-    return ' '.join(tokens)
+dax_merged['sentiment'] = dax_merged['title'].apply(finvader, use_sentibignomics = True, use_henry = True, indicator="compound")   
 
-dax_merged['processed_title'] = dax_merged['title'].apply(preprocess_text)
-
-def vader_sentiment(text):
-    return analyzer.polarity_scores(text)['compound']
-
-dax_merged['sentiment'] = dax_merged['processed_title'].apply(vader_sentiment)
+dax_merged.to_csv('sentiments_merged.csv') 
